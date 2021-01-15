0e3d959ee486772b78659303e6c0334131843b05
Lu.Xu <thusimon@gmail.com>
2021-01-15 15:01:48 -0800

    add laser dender game

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,13 @@
 require('dotenv').config();
 import path from 'path';
 import express from 'express';
-import WebSocket from "ws"
+import WebSocket from 'ws';
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+mongoose.set('useFindAndModify', false);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+const CONNECTION_URI = process.env.MONGODB_URI!;
 
 /* ------interface------- */
 enum MessageType {
@@ -33,20 +39,80 @@ interface BulletsData {
   [key: string]: Position[]
 }
 
+/* ------db------ */
+const connectToDb = () => {
+  return mongoose.connect(CONNECTION_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }).then(async () => {
+    console.log(`Connected to mongoDB to ${CONNECTION_URI}`);
+    return Promise.resolve();
+  });
+};
+
+const LaserScoreSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  credit: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps:true
+});
+
+const LaserScore = mongoose.model('LaserScore', LaserScoreSchema);
 /* ------server------- */
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.static(path.join(__dirname, '../../client/build')));
+const router = express.Router();
 
-app.get('/', function(req, res) {
+router.get('/api/lasercredits', async (req, res) => {
+  try {
+    const laserScores = await LaserScore.find({}, {_id: 0, updatedAt: 0, createdAt: 0, __v: 0}).sort({credit: -1});
+    return res.status(200).json({credits: laserScores});
+  } catch (e) {
+    return res.status(400).json({err: e.message})
+  }
+});
+
+router.get('/api/lasercredit', async (req, res) => {
+  const {name, credit} = req.query;
+  if (!name || !credit) {
+    return res.status(400).json({err: 'missing param'});
+  }
+  const creditInt = Number.parseInt(credit as string);
+  try {
+    const updateResult = await LaserScore.findOneAndUpdate({name}, {name, credit: creditInt}, {new: true, upsert: true});
+    return res.status(200).json({credit: updateResult});
+  } catch (e) {
+    return res.status(400).json({err: e.message});
+  }
+});
+
+app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, '../../client/static')));
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
-const server = app.listen(PORT, () => {
-  console.log(`Successfully started server on port ${PORT}`);
+app.get('/laserdefender', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/static/laserDefender/index.html'));
 });
+
+app.use(router);
+
+const server = app.listen(PORT, () => {
+  connectToDb().then(() => {
+    console.log(`Successfully started server on port ${PORT}`);
+  });
+});
+
 
 /* ------websocket------- */
 const wss = new WebSocket.Server({
