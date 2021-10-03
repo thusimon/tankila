6e101d498f6b5ec6619c0d2ef3b42dc2aaa03d3b
Lu Xu <thusimon@gmail.com>
2021-10-02 23:55:45 -0700

    adjust camera and movement

--- client/src/client.ts
+++ client/src/client.ts
@@ -10,13 +10,15 @@ const light = new THREE.AmbientLight()
 scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  60,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 )
-camera.position.y = 1
-camera.position.z = 2
+
+camera.position.y = 1.5
+camera.position.x = -2
+camera.lookAt(new THREE.Vector3(10, 0, 0));
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -25,11 +27,11 @@ document.body.appendChild(renderer.domElement)
 const menuPanel = document.getElementById('menuPanel') as HTMLDivElement
 const startButton = document.getElementById('startButton') as HTMLInputElement
 startButton.addEventListener(
-    'click',
-    function () {
-        controls.lock()
-    },
-    false
+  'click',
+  function () {
+    controls.lock()
+  },
+  false
 )
 
 const controls = new PointerLockControls(camera, renderer.domElement)
@@ -39,70 +41,41 @@ controls.addEventListener('unlock', () => (menuPanel.style.display = 'block'))
 
 const planeGeometry = new THREE.PlaneGeometry(100, 100, 50, 50)
 const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
+  color: 0x00ff00,
+  wireframe: true,
 })
 const plane = new THREE.Mesh(planeGeometry, material)
 plane.rotateX(-Math.PI / 2)
 scene.add(plane)
 
-const cubes: THREE.Mesh[] = []
-for (let i = 0; i < 100; i++) {
-    const geo = new THREE.BoxGeometry(
-        Math.random() * 4,
-        Math.random() * 16,
-        Math.random() * 4
-    )
-    const mat = new THREE.MeshBasicMaterial({ wireframe: true })
-    switch (i % 3) {
-        case 0:
-            mat.color = new THREE.Color(0xff0000)
-            break
-        case 1:
-            mat.color = new THREE.Color(0xffff00)
-            break
-        case 2:
-            mat.color = new THREE.Color(0x0000ff)
-            break
-    }
-    const cube = new THREE.Mesh(geo, mat)
-    cubes.push(cube)
-}
-cubes.forEach((c) => {
-    c.position.x = Math.random() * 100 - 50
-    c.position.z = Math.random() * 100 - 50
-    c.geometry.computeBoundingBox()
-    c.position.y =
-        ((c.geometry.boundingBox as THREE.Box3).max.y -
-            (c.geometry.boundingBox as THREE.Box3).min.y) /
-        2
-    scene.add(c)
-})
-
 const onKeyDown = function (event: KeyboardEvent) {
-    switch (event.code) {
-        case 'KeyW':
-            controls.moveForward(0.1)
-            break
-        case 'KeyA':
-            controls.moveRight(-0.1)
-            break
-        case 'KeyS':
-            controls.moveForward(-0.1)
-            break
-        case 'KeyD':
-            controls.moveRight(0.1)
-            break
-    }
+  const tank = scene.children[3] as THREE.Object3D;
+  switch (event.code) {
+    case 'KeyW':
+      controls.moveForward(0.1);
+      tank.position.x += 0.1;
+      break
+    case 'KeyA':
+      //controls.moveRight(-0.1)
+      tank.rotation.z += 0.02;
+      break
+    case 'KeyS':
+      controls.moveForward(-0.1)
+      tank.position.x -= 0.1;
+      break
+    case 'KeyD':
+      //controls.moveRight(0.1)
+      tank.rotation.z -= 0.02;
+      break
+  }
 }
-document.addEventListener('keydown', onKeyDown, false)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  render()
 }
 
 const stats = Stats()
@@ -110,23 +83,26 @@ document.body.appendChild(stats.dom)
 
 const loader = new GLTFLoader();
 loader.load('./models/styled_tank/scene.gltf', function(gltf){
-  const car = gltf.scene.children[0];
-  car.scale.set(0.5,0.5,0.5);
-  scene.add(gltf.scene);
+  const tank = gltf.scene.children[0];
+  tank.scale.set(0.3,0.3,0.3);
+  tank.rotation.z += Math.PI / 2;
+  scene.add(tank);
+  console.log(scene);
+  document.addEventListener('keydown', onKeyDown, false)
 });
 
 function animate() {
-    requestAnimationFrame(animate)
+  requestAnimationFrame(animate)
 
-    //controls.update()
+  //controls.update()
 
-    render()
+  render()
 
-    stats.update()
+  stats.update()
 }
 
 function render() {
-    renderer.render(scene, camera)
+  renderer.render(scene, camera)
 }
 
 animate()
