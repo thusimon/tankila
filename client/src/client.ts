537fa2857aaefdbe8bd2e13988c4cb9778e2093e
Lu Xu <thusimon@gmail.com>
2021-10-02 19:23:17 -0700

    adding PointerLock control and tank model

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,9 +1,13 @@
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import Stats from 'three/examples/jsm/libs/stats.module';
-import {GUI} from 'three/examples/jsm/libs/dat.gui.module';
-const scene = new THREE.Scene()
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
+const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
+
+const light = new THREE.AmbientLight()
+scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -11,59 +15,113 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
+camera.position.y = 1
 camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement);
+const menuPanel = document.getElementById('menuPanel') as HTMLDivElement
+const startButton = document.getElementById('startButton') as HTMLInputElement
+startButton.addEventListener(
+    'click',
+    function () {
+        controls.lock()
+    },
+    false
+)
 
-const geometry = new THREE.BoxGeometry()
+const controls = new PointerLockControls(camera, renderer.domElement)
+//controls.addEventListener('change', () => console.log("Controls Change"))
+controls.addEventListener('lock', () => (menuPanel.style.display = 'none'))
+controls.addEventListener('unlock', () => (menuPanel.style.display = 'block'))
+
+const planeGeometry = new THREE.PlaneGeometry(100, 100, 50, 50)
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
+const plane = new THREE.Mesh(planeGeometry, material)
+plane.rotateX(-Math.PI / 2)
+scene.add(plane)
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-scene.add(new THREE.AxesHelper(5))
+const cubes: THREE.Mesh[] = []
+for (let i = 0; i < 100; i++) {
+    const geo = new THREE.BoxGeometry(
+        Math.random() * 4,
+        Math.random() * 16,
+        Math.random() * 4
+    )
+    const mat = new THREE.MeshBasicMaterial({ wireframe: true })
+    switch (i % 3) {
+        case 0:
+            mat.color = new THREE.Color(0xff0000)
+            break
+        case 1:
+            mat.color = new THREE.Color(0xffff00)
+            break
+        case 2:
+            mat.color = new THREE.Color(0x0000ff)
+            break
+    }
+    const cube = new THREE.Mesh(geo, mat)
+    cubes.push(cube)
+}
+cubes.forEach((c) => {
+    c.position.x = Math.random() * 100 - 50
+    c.position.z = Math.random() * 100 - 50
+    c.geometry.computeBoundingBox()
+    c.position.y =
+        ((c.geometry.boundingBox as THREE.Box3).max.y -
+            (c.geometry.boundingBox as THREE.Box3).min.y) /
+        2
+    scene.add(c)
+})
+
+const onKeyDown = function (event: KeyboardEvent) {
+    switch (event.code) {
+        case 'KeyW':
+            controls.moveForward(0.1)
+            break
+        case 'KeyA':
+            controls.moveRight(-0.1)
+            break
+        case 'KeyS':
+            controls.moveForward(-0.1)
+            break
+        case 'KeyD':
+            controls.moveRight(0.1)
+            break
+    }
+}
+document.addEventListener('keydown', onKeyDown, false)
+
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
 }
 
-const stats = Stats();
-document.body.appendChild(stats.dom);
+const stats = Stats()
+document.body.appendChild(stats.dom)
 
-const gui = new GUI();
-const cubeFolder = gui.addFolder('Cube');
-const cubeRotationFolder = cubeFolder.addFolder('Rotation');
-cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI*2);
-cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI*2);
-cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI*2);
-cubeRotationFolder.open();
-const cubePositionFolder = cubeFolder.addFolder('Position');
-cubePositionFolder.add(cube.position, 'x', -10, 10);
-cubePositionFolder.add(cube.position, 'y', -10, 10);
-cubePositionFolder.add(cube.position, 'z', -10, 10);
-cubePositionFolder.open();
-const cubeScaleFolder = cubeFolder.addFolder('Scale');
-cubeScaleFolder.add(cube.scale, 'x', -5, 5);
-cubeScaleFolder.add(cube.scale, 'y', -5, 5);
-cubeScaleFolder.add(cube.scale, 'z', -5, 5);
-cubeScaleFolder.open();
-cubeFolder.add(cube, 'visible')
-cubeFolder.open();
-const cameraFolder = gui.addFolder('Camera');
-cameraFolder.add(camera.position, 'z', 0, 10);
-cameraFolder.open();
+const loader = new GLTFLoader();
+loader.load('./models/styled_tank/scene.gltf', function(gltf){
+  const car = gltf.scene.children[0];
+  car.scale.set(0.5,0.5,0.5);
+  scene.add(gltf.scene);
+});
 
 function animate() {
     requestAnimationFrame(animate)
+
+    //controls.update()
+
     render()
+
     stats.update()
 }
 
