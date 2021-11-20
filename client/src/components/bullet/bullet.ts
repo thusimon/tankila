a3e7c6cf51b80839cb2fa6ed5ed555b951c3dae4
Lu Xu <thusimon@gmail.com>
2021-11-19 18:14:24 -0800

    code cleaning

--- client/src/components/bullet/bullet.ts
+++ client/src/components/bullet/bullet.ts
@@ -1,16 +1,8 @@
-import * as THREE from 'three'
-import * as CANNON from 'cannon-es'
-import {BULLET_SPEED} from '../../utils/constants';
-import { UserBody } from '../../types/Types';
-import Explosion from './explosion';
-import Tank from '../tank/tank';
+import * as THREE from 'three';
 
 class Bullet {
   model: THREE.Mesh;
   scene: THREE.Scene
-  //bulletsToRemove: Bullet[];
-  //explosions: Explosion[] = [];
-  removeFlag: boolean = false;
   constructor(scene: THREE.Scene, x:number, y: number, z: number) {
     this.scene = scene;
     const bulletGeo = new THREE.SphereGeometry(0.1, 8, 8); 
@@ -23,27 +15,11 @@ class Bullet {
 
   removeBullet() {
     this.scene.remove(this.model);
-    this.removeFlag = true;
   }
 
   addBullet() {
     this.scene.add(this.model);
-    this.removeFlag = false;
   }
-
-  // bulletExplode() {
-  //   const explosion = new Explosion(new THREE.Color(0xffff00), this.scene);
-  //   this.explosions.push(explosion);
-  //   explosion.explode(new THREE.Vector3(this.bulletBody.position.x, this.bulletBody.position.y, this.bulletBody.position.z));
-  // }
-
-  // updateSphere() {
-  //   this.bulletSphere.position.set(
-  //     this.bulletBody.position.x,
-  //     this.bulletBody.position.y,
-  //     this.bulletBody.position.z
-  //   );
-  // }
 }
 
 export default Bullet;
