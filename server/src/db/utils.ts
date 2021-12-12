0ff31354ddfe3e6bb75ee193362861f7f2dbe56b
Lu Xu <thusimon@gmail.com>
2021-12-12 13:51:42 -0800

    fix score API failure

--- server/src/db/utils.ts
+++ server/src/db/utils.ts
@@ -1,10 +1,11 @@
-import {Model, FilterQuery, UpdateQuery, Document, Query, EnforceDocument} from 'mongoose';
+import {Model, FilterQuery, UpdateQuery } from 'mongoose';
 
-export const getAll = async <T>(mongoModel: Model<T>): Query<Array<EnforceDocument<T, unknown, unknown>>, Document>  => {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const getAll = async <T>(mongoModel: Model<T>)  => {
   return await mongoModel.find({}, {_id: 0, updatedAt: 1, createdAt: 1, __v:1, name: 1, credit: 1}).sort({credit: -1});
 };
 
-export const updateOne = async <T>(mongoModel: Model<T>, filterQuery: Partial<Record<keyof T, unknown>>, updateQuery: Partial<Record<keyof T, unknown>>)
-  : Query<EnforceDocument<T, unknown, unknown>, Document> => {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const updateOne = async <T>(mongoModel: Model<T>, filterQuery: Partial<Record<keyof T, unknown>>, updateQuery: Partial<Record<keyof T, unknown>>) => {
   return await mongoModel.findOneAndUpdate(filterQuery as FilterQuery<T>, updateQuery as UpdateQuery<T>, {new: true, upsert: true});
 };
