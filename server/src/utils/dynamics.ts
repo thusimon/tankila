0a50304596df4fa58cef30b17548c8ae8d7dad88
Lu Xu <thusimon@gmail.com>
2022-10-16 09:35:29 -0700

    update packages

--- server/src/utils/dynamics.ts
+++ server/src/utils/dynamics.ts
@@ -12,7 +12,7 @@ export const generateRandomPosition = (lowerBound: CANNON.Vec3, upperBound: CANN
 }
 
 export const randomEnum = <T>(anEnum: T): T[keyof T] => {
-  const enumValues = Object.keys(anEnum)
+  const enumValues = Object.keys(anEnum as object)
     .map(n => Number.parseInt(n))
     .filter(n => !Number.isNaN(n)) as unknown as T[keyof T][];
   const randomIndex = Math.floor(Math.random() * enumValues.length);
